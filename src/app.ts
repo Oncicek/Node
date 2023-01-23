6e86fc2ffadbb6a6aff08959189e01e30f1adc6a
Tomáš Králík <tomas.kralik@snowlycode.com>
2023-01-23 17:15:22 +0100

    first commit

---
+++ src/app.ts
@@ -0,0 +1,26 @@
+import { NextFunction, Request, Response } from "express";
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+app.use(bodyParser.json());
+
+app.post("/kocka", (req: Request, res: Response, next: NextFunction) => {
+  console.log(req.body);
+  res.sendStatus(200);
+});
+
+app.use("/", (req: Request, res: Response, next: NextFunction) => {
+  console.log("druhej");
+  res.send("<div>TADY JE VSETKO!!</div>");
+});
+
+app.listen(3000);
